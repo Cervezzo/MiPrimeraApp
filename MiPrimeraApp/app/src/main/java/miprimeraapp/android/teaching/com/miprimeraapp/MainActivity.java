6ce1a76be613f145e9a9a6c534b5b78cf01f3230
Antonio J. Castillo <antoniocasma@gmail.com>
2018-05-09 09:26:05 +0200

    actualización (pruebas varias)

--- MiPrimeraApp/app/src/main/java/miprimeraapp/android/teaching/com/miprimeraapp/MainActivity.java
+++ MiPrimeraApp/app/src/main/java/miprimeraapp/android/teaching/com/miprimeraapp/MainActivity.java
@@ -1,13 +1,80 @@
 package miprimeraapp.android.teaching.com.miprimeraapp;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.content.Intent;
+        import android.drm.DrmStore;
+        import android.media.MediaPlayer;
+        import android.net.Uri;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.util.Log;
+        import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    public void onClick(View view){
+
+        Intent intent = new Intent(this, SecondActivity.class);
+
+    }
+
+    public void onClick1(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("https://giphy.com/explore/biblioteca"));
+
+    startActivity(intent);}
+
+    public void onClick2(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("tel:1234"));
+        startActivity(intent);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("MainActivity", "ANTONIO: onCreate");
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("MainActivity", "ANTONIO: onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("MainActivity", "ANTONIO: onResume");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("MainActivity", "ANTONIO: onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("MainActivity", "ANTONIO: onStop");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d("MainActivity","ANTONIO: onDestroy");
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.d("MainActivity","ANTONIO: onRestart");
     }
 }
---
+++ RelativeL/app/src/main/java/tarkov/android/teaching/com/relativel/RelativeL.java
@@ -0,0 +1,13 @@
+package tarkov.android.teaching.com.relativel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RelativeL extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative_l);
+    }
+}
